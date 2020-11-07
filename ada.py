0a63ccd2034fdbe244dfc8f250ce96a0d7d518a2
Stephen Hulme <shulme801@gmail.com>
2020-11-07 10:53:12 -0500

    Commit 3 7 Nov

--- ada.py
+++ ada.py
@@ -1,4 +1,5 @@
 first_name = "ada"
 last_name = "lovelace"
-full_name = f" {first_name} {last_name}"
-print(f"Hello,{full_name.title()}!")
+full_name = f" {first_nCoame} {last_name}"
+message = f"Hello,{full_name.title()}!"
+print(message)
