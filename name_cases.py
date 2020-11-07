ede91a3ef45d1f169e4b980721675682e520e1f0
Stephen Hulme <shulme801@gmail.com>
2020-11-07 11:32:07 -0500

    5th commit 7 Nov

---
+++ name_cases.py
@@ -0,0 +1,6 @@
+first_name = "stephen"
+sur_name = "hulme"
+full_name = f"{first_name} {sur_name}"
+lang = "python"
+message = f"Hello, {full_name.title()}.  Would you like to learn some {lang.title()} today?"
+print(message)
