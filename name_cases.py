2b3464a6f31e7d8c800832382b9d59ae98b579f0
Stephen Hulme <shulme801@gmail.com>
2020-11-08 13:08:14 -0500

    8 Nov Beta

--- name_cases.py
+++ name_cases.py
@@ -8,11 +8,9 @@ print(full_name.upper())
 print(full_name.lower())
 print(full_name.title())
 famous_person = 'mark twain'
-quotation=""
+quotation = ""
 message = f"{famous_person.title()} once said"
 quotation = "'Everyone complains about the weather, but nobody does anything about it!'"
-myTuple = (message,quotation)
-myTuple=" ".join(myTuple)
+myTuple = (message, quotation)
+myTuple = " ".join(myTuple)
 print(myTuple)
-
-
