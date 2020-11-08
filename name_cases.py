8d8d83771b20e19a2b0121f8606b800a69587076
Stephen Hulme <shulme801@gmail.com>
2020-11-08 13:03:54 -0500

    8 Nov alpha

--- name_cases.py
+++ name_cases.py
@@ -4,3 +4,15 @@ full_name = f"{first_name} {sur_name}"
 lang = "python"
 message = f"Hello, {full_name.title()}.  Would you like to learn some {lang.title()} today?"
 print(message)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+famous_person = 'mark twain'
+quotation=""
+message = f"{famous_person.title()} once said"
+quotation = "'Everyone complains about the weather, but nobody does anything about it!'"
+myTuple = (message,quotation)
+myTuple=" ".join(myTuple)
+print(myTuple)
+
+
---
+++ number8.py
@@ -0,0 +1,6 @@
+print(5+3)
+print(10-2)
+print(2**3)
+print(32/4)
+fave=7
+print
