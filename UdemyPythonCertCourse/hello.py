34f599881bcaf21a2a990d47ad2cc116f0fdf9c1
Stephen Hulme <shulme801@gmail.com>
2021-01-12 18:34:46 -0500

    12 Jan C

--- UdemyPythonCertCourse/hello.py
+++ UdemyPythonCertCourse/hello.py
@@ -3,3 +3,28 @@ print(foobar)
 
 print(type(True))
 # Python is a dynamically-typed language
+
+answer = 10 + 3 * 9 - 4
+print(answer)
+answer = (10 + 3) * (9 - 4)
+print(answer)
+
+sentence = 'This is a sentence'
+print(sentence[0:4])
+print(sentence[5:7])
+# print(sentence[-2])
+sentence = "abcdefg"
+print("\n")
+print(sentence[0::2])
+sen = sentence.upper()
+print(sentence)
+print(sen)
+
+sentence = "this IS a 99484 sentence"
+print(sentence.capitalize())
+
+sentence = "A94848%$3947343983"
+print(sentence.startswith("8%$", 5))
+
+sentence = "\nThe sum of {0} + {2} is {1}".format(50, 55, 5)
+print(sentence)
