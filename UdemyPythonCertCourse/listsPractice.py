43603d1a34c15ca2905da858e33b88e555c49858
Stephen Hulme <shulme801@gmail.com>
2021-01-17 12:19:56 -0500

    17 Jan E

--- UdemyPythonCertCourse/listsPractice.py
+++ UdemyPythonCertCourse/listsPractice.py
@@ -9,9 +9,17 @@ print("\n my_List is now {0}\n".format(my_List))
 sentence = my_List.pop()
 print("\nJust popped my_List's last element and my_List is now {0}".format(my_List)+"\n")
 print("\nAnd sentence is {0}".format(sentence))
-"""
 myList = [4,5,3,1,2]
 print("\nmyList is initially {0}\n".format(myList))
 myList.sort()
+howdy = myList.reverse()
+print("\nAfter sort and reverse, my list is {0}\n".format(myList))
+print("\nand the reverse function call returned {0}\n".format(howdy))
+"""
+# Try reverse and sort methods with string elements in the list
+myList = ['a','c','q','ce','d','b','bq','qbed','qaed','z']
+print("\nmyList is initially {0}\n".format(myList))
+#
+howdy = myList.sort()
 print("\nAfter sort, my list is {0}\n".format(myList))
-print(myList)
+print("\nand the sort function call returned {0}\n".format(howdy))
