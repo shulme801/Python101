3f39003eb7d44abfbf0ecd162f7d2e94beefde1b
Stephen Hulme <shulme801@gmail.com>
2021-01-22 10:17:25 -0500

    22 Jan D

--- UdemyPythonCertCourse/Section2.py
+++ UdemyPythonCertCourse/Section2.py
@@ -1,3 +1,5 @@
+import re
+
 my_list = ['a', 'b', 'c', 1, 2, 3, ['apple', 'orange', ['john', 'robert'], 'banana'], 'd']
 my_list[6][2][1] = 'joe'
 print("\nHere's the new name {0}\n".format(my_list[6][2][1]))
