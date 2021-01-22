7556c9abb05201fd037703bbcd355a9cefa5efdb
Stephen Hulme <shulme801@gmail.com>
2021-01-22 08:38:16 -0500

    22 Jan B

--- UdemyPythonCertCourse/Section2.py
+++ UdemyPythonCertCourse/Section2.py
@@ -1,2 +1,3 @@
 my_list = ['a', 'b', 'c', 1, 2, 3, ['apple', 'orange', ['john', 'robert'], 'banana'], 'd']
-print(my_list[6][2][1])
+my_list[6][2][1] = 'joe'
+print("\nHere's the new name {0}\n".format(my_list[6][2][1]))
