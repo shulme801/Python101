b1d8db8f859a62f584d81182e679dca46680e587
Stephen Hulme <shulme801@gmail.com>
2021-01-22 11:01:45 -0500

    22 Jan E

--- UdemyPythonCertCourse/Section2.py
+++ UdemyPythonCertCourse/Section2.py
@@ -3,3 +3,9 @@ import re
 my_list = ['a', 'b', 'c', 1, 2, 3, ['apple', 'orange', ['john', 'robert'], 'banana'], 'd']
 my_list[6][2][1] = 'joe'
 print("\nHere's the new name {0}\n".format(my_list[6][2][1]))
+newStr = str(my_list)
+print("\nHere's newStr {0}".format(newStr))
+myTuple = (1, 2, 3, "Some Data", [1, 2, 3])
+myTuple[4][1] = 99
+print(myTuple[4][1])
+print("\nmyTuple is now {0}\n".format(myTuple))
