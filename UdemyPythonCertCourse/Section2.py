80ecd99b0db8f1e8f85c6cc7fffe7f94cf82db22
Stephen Hulme <shulme801@gmail.com>
2021-01-22 11:08:00 -0500

    22 Jan F

--- UdemyPythonCertCourse/Section2.py
+++ UdemyPythonCertCourse/Section2.py
@@ -5,7 +5,9 @@ my_list[6][2][1] = 'joe'
 print("\nHere's the new name {0}\n".format(my_list[6][2][1]))
 newStr = str(my_list)
 print("\nHere's newStr {0}".format(newStr))
-myTuple = (1, 2, 3, "Some Data", [1, 2, 3])
-myTuple[4][1] = 99
-print(myTuple[4][1])
+myTuple = (1, 2, 3, "foobar", [1, 2, 3], "foobar")
+# myTuple[3] = "foobar"
+myTuple [4][1] = "foobar"
 print("\nmyTuple is now {0}\n".format(myTuple))
+count = myTuple.count("foobar")
+print("\nNumber of 'foobars' is {0}\n".format(count))
