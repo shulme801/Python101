abda58e83d3da57440dd172535336d2769d4d136
Stephen Hulme <shulme801@gmail.com>
2021-01-22 17:54:27 -0500

    22 Jan G

--- UdemyPythonCertCourse/PythonCourse-master/Section_02/assignment_01.py
+++ UdemyPythonCertCourse/PythonCourse-master/Section_02/assignment_01.py
@@ -1,13 +1,10 @@
 # Assignment 1:
 """
 Print Bill's salary from the my_list object shown below.
-
-my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
-
 """
-
-
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
 # your code below:
+print(my_list[0].get('Bill'))
 
 
 
