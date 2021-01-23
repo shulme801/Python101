2384493090662c811fe3f1e781d637729e7ac0aa
Stephen Hulme <shulme801@gmail.com>
2021-01-23 12:39:44 -0500

    23 Jan D

--- UdemyPythonCertCourse/PythonCourse-master/Section_02/assignment_04.py
+++ UdemyPythonCertCourse/PythonCourse-master/Section_02/assignment_04.py
@@ -6,12 +6,14 @@ and replace the word TV with the word television. Then print my_list.
 
 my_list = [(1, 2), (3, 4), (['c', 'd', 'a', 'm'], [3, 9, 4, 12], 4), 'TV', 42]
 
-
-my_list[2][0][3] = 'x'
-my_list[3] = 'Television'
-
+my_list[2][0][3] = "x"
+my_list[3]= "television"
 print(my_list)
 
+old_tuple = my_list.pop(2)
+print(old_tuple)
+el_1 = old_tuple[0]
+print(el_1)
 
 
 
