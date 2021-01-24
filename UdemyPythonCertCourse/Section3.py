1ffc36ee7f7f655a5cb7743326e7f1c9a1459770
Stephen Hulme <shulme801@gmail.com>
2021-01-24 11:31:43 -0500

    24 Jan B

--- UdemyPythonCertCourse/Section3.py
+++ UdemyPythonCertCourse/Section3.py
@@ -1,5 +1,25 @@
 def greet_person(person_name = "buddy"):
-    print("\nHello there {0}! How are you?\n".format(person_name))
+    """
+        DOCSTRING: Print a personalized greeting.
+        INPUT: person_name
+        OUTPUT: hello...person_name
+    """
+    print("\nHello there {0}! How are you?\n".format(str(person_name)))
 
-greet_person()
+def remainder(dividend = 0, divisor = 1):
+    """
+        DOCSTRING: Divide dividend by divisor and return the remainder.
+        INPUT: dividend: converted to int
+        INPUT: divisor: converted to int
+        OUTPUT: remainder: int
+    """
+    foo = int(dividend)
+    bar = int(divisor)
+    return(int(foo % bar))
+    
+
+print(greet_person.__doc__)
+greet_person(99)
 greet_person("Stephen")
+print(remainder.__doc__)
+print("\nHere's the remainder function, 42 mod 11 = {0}\n".format(remainder(42,11)))
