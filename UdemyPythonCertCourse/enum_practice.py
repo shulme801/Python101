c63596669935e082d647b3afc14ed6ddf525efe8
Stephen Hulme <shulme801@gmail.com>
2021-01-27 15:55:58 -0500

    27 Jan A

--- UdemyPythonCertCourse/enum_practice.py
+++ UdemyPythonCertCourse/enum_practice.py
@@ -1,7 +1,18 @@
+'''
 my_nums = [1, 2, 3, 4, 5]
 words   = ['hello', 'my', 'name', 'is', 'stephen']
 
 for item in zip(my_nums, words):
+    print(item)
+'''
+
+my_nums = [1, 2, 3, 4, 5, 6, 7, 8]
+words   = ['hello', 'my', 'name', 'is', 'stephen']
+for item in zip(my_nums, words):
+    print(item)
+
+print("\n\n")
+for item in enumerate(words,1):
     print(item)
 
 print("\nThat's all, Folks\n")
