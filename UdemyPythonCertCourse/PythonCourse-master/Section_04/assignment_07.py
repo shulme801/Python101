5cf27e7c728167653cb7d1aeb9ce77216d42fbc8
Stephen Hulme <shulme801@gmail.com>
2021-01-31 11:43:40 -0500

    31 Jan E

--- UdemyPythonCertCourse/PythonCourse-master/Section_04/assignment_07.py
+++ UdemyPythonCertCourse/PythonCourse-master/Section_04/assignment_07.py
@@ -11,7 +11,36 @@ EXAMPLE:
     string_match('abc', 'axc') → 0
 
 """
+def string_match(str_a, str_b):
+    '''
+    DOCSTRING:
+    '''
+    str_a = str(str_a)
+    str_b = str(str_b)
 
+    # find the length of the shortest string
+    shortest = min(len(str_a), len(str_b))
+    count    = 0
+    if (shortest >= 2):
+        for i in range(shortest-1): # make 2 char substrings from pos {0,1}
+            if (str_a[i:i+2] == str_b[i:i+2]):
+                count +=1
+    return(count)
+
+
+
+print(string_match('xxcaazz','xxbaaz'))
+print(string_match('abc','abc'))
+print(string_match('abc','axc'))
+
+
+
+
+
+
+
+
+'''
 def string_match(a, b):
 
     # Figure out which string is shorter
@@ -26,9 +55,7 @@ def string_match(a, b):
             count = count + 1
 
     return count
-
-
-
+'''
 
 
 
