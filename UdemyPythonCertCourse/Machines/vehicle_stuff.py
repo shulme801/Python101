55a707b0b830fecd8fdc52aca881ae6caa9cceb1
Stephen Hulme <shulme801@gmail.com>
2021-02-06 12:13:04 -0500

    6 Feb A

--- UdemyPythonCertCourse/Machines/vehicle_stuff.py
+++ UdemyPythonCertCourse/Machines/vehicle_stuff.py
@@ -6,9 +6,15 @@ class Vehicle:
     def __init__ (self, body_type, make):
         self.vehicle_body = body_type
         self.vehicle_make = make
-        Vehicle.vehicle_counter +=1
+        Vehicle.vehicle_counter += 1
 
     def get_vehicle_count(self):
         return Vehicle.vehicle_counter
 
-    
+    def drive(self):
+        print("Vehicle driving!")
+
+class Truck(Vehicle):
+    def drive(self):
+        print("truck driving")
+       
--- UdemyPythonCertCourse/car_app.py
+++ UdemyPythonCertCourse/car_app.py
@@ -1,3 +1,9 @@
-from machines.vehicle_stuff import Vehicle
+from machines.vehicle_stuff import Vehicle, Truck
 
-car1 = Vehicle('sedan', 'Toyota')
+# car1   = Vehicle('sedan', 'Toyota')
+truck1 = Truck("Big Rig", "Mercedes")
+truck2 = Truck("Small Rig", "Chevy")
+truck3 = Truck("Big Rig", "Toyota")
+
+print("Here's the vehicle count after truck3 is created {0}".format(truck3.get_vehicle_count()))
+truck1.drive()
